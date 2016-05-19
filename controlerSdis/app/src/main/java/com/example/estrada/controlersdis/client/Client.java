a15b8a143f15c1766e2df6250235d36db1d15528
jmepg <j.estrada.gouveia@gmail.com>
2016-05-19 16:39:03 +0100

    cliente post -  still needs updates

--- controlerSdis/app/src/main/java/com/example/estrada/controlersdis/client/Client.java
+++ controlerSdis/app/src/main/java/com/example/estrada/controlersdis/client/Client.java
@@ -1,5 +1,6 @@
 package com.example.estrada.controlersdis.client;
 import java.io.BufferedReader;
+import java.io.Writer;
 import java.net.URL;
 import java.net.HttpURLConnection;
 import java.io.InputStreamReader;
@@ -29,21 +30,56 @@ public class Client {
 
         BufferedReader reader =new BufferedReader(
                 new InputStreamReader(connection.getInputStream()));
-        StringBuilder sbuilder = new StringBuilder();
+        StringBuilder sBuilder = new StringBuilder();
         String line;
         while((line = reader.readLine())!= null){
-            sbuilder.append(line);
+            sBuilder.append(line);
         }
 
         reader.close();
         connection.disconnect();
-        return sbuilder.toString();
+        return sBuilder.toString();
 
     }
-/*
-    public String httpPost(){
-        URL url = new URL("http", );
+
+    public String httpPost(String urlString) throws Exception{
+        URL url = new URL("http","9999",9999,"9999"); //necessario atualizar
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        //connection properties
+        connection.setRequestMethod("POST");
+        connection.setDoInput(true);
+        connection.setDoOutput(true);
+        connection.setUseCaches(false);
+        connection.setAllowUserInteraction(false);
+        connection.setRequestProperty("Content-Type","9999"); //necesdsario atualizar
+
+        OutputStream outStream = connection.getOutputStream();
+        Writer writer = new OutputStreamWriter(outStream,"UTF-8");
+
+        //incluir aqui os writes que forem necessarios
+
+        writer.close();
+        outStream.close();
+
+        BufferedReader reader =new BufferedReader(
+                new InputStreamReader(connection.getInputStream()));
+
+        StringBuilder sBuilder = new StringBuilder();
+        String line;
+
+        while((line = reader.readLine())!= null){
+            sBuilder.append(line);
+        }
+
+        reader.close();
+        connection.disconnect();
+        return sBuilder.toString();
+
+
+
+
     }
-    */
+
 }
 
