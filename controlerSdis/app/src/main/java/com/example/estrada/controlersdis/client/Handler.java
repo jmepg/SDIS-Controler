567ebc56c38362a0bdfa4853e608e413344efdbc
Aramiis <andrecmmaia@gmail.com>
2016-05-21 19:21:24 +0100

    não sei como tratar das sms

--- controlerSdis/app/src/main/java/com/example/estrada/controlersdis/client/Handler.java
+++ controlerSdis/app/src/main/java/com/example/estrada/controlersdis/client/Handler.java
@@ -1,5 +1,31 @@
 package com.example.estrada.controlersdis.client;
 
+import java.io.OutputStream;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.InputStream;
 
-public class Handler{
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+
+class Handler implements HttpHandler {
+
+    private Listener listener;
+
+    public Handler(Listener listener) {
+        this.listener = listener;
+    }
+
+    public void handler(HttpExchange httpexchange) throws IOException {
+
+        InputStream is = httpexchange.getRequestBody();
+
+        Scanner scanner = new Scanner(is, "UTF-8").useDelimiter("\n");
+
+        String path = httpexchange.getRequestURI().getPath();
+
+       ////MISSING CODE HERE
+        //regarding messages
+    }
 }
